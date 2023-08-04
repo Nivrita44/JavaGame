8a7b29d0b8f13a30a4c8d7a06e8976bd5eae05af
Nivrita44 <107574615+Nivrita44@users.noreply.github.com>
2023-08-04 11:36:30 +0600

    1st part

--- PingPongGame/src/SimplePong/Game.java
+++ PingPongGame/src/SimplePong/Game.java
@@ -1,4 +1,48 @@
 package SimplePong;
+import java.awt.*;
 
-public class Game {
+public class Game extends Canvas implements Runnable {
+
+    public static final int WIDTH=1000;
+    public static final int HEIGHT=WIDTH * 9/16;
+    public boolean running=false;
+    private Thread gameThread;
+    private Ball ball;
+    private Paddle paddle1;
+    private Paddle paddle2;
+    public Game() {
+        canvasSetup();
+        new Window("Ping Pong",this);
+
+
+    }
+    private void canvasSetup() {
+        this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        this.setMaximumSize(new Dimension(WIDTH,HEIGHT));
+        this.setMinimumSize(new Dimension(WIDTH,HEIGHT));
+    }
+
+    @Override
+    public void run() {
+
+    }
+    public void start(){
+        gameThread=new Thread(this);
+        gameThread.start();
+        running=true;
+
+    }
+    public void stop(){
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+    public static void main(String [] args){
+        new Game();
+    }
+
 }
--- PingPongGame/src/SimplePong/Window.java
+++ PingPongGame/src/SimplePong/Window.java
@@ -5,5 +5,13 @@ public class Window {
     public Window(String title,Game game)
     {
         JFrame frame=new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.add(game);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        game.start();
     }
 }
